8928bb141709d040ec09049fc9c844e614414948
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2018-07-20 14:38:18 -0700

    correct path to package

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../py-hostlist/hostlist.py'))
-
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
