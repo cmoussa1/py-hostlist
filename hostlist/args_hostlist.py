0186c68d406b28429fd2f8407e099a495141bd02
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2018-07-20 14:35:27 -0700

    wrap code in `if __name__ == "__main__"`

--- hostlist/args_hostlist.py
+++ hostlist/args_hostlist.py
@@ -33,41 +33,43 @@ def nth(args):
 	param2 = args.param2
 	hl.nth(param1, param2)
 
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers()
 
-expand_parser = subparsers.add_parser('expand')
-expand_parser.add_argument('param1')
-expand_parser.set_defaults(func=expand)
+if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers()
 
-compress_range_parser = subparsers.add_parser('compr')
-compress_range_parser.add_argument('param1')
-compress_range_parser.set_defaults(func=compress_range)
+        expand_parser = subparsers.add_parser('expand')
+        expand_parser.add_argument('param1')
+        expand_parser.set_defaults(func=expand)
 
-compress_parser = subparsers.add_parser('comp')
-compress_parser.add_argument('param1')
-compress_parser.set_defaults(func=compress)
+        compress_range_parser = subparsers.add_parser('compr')
+        compress_range_parser.add_argument('param1')
+        compress_range_parser.set_defaults(func=compress_range)
 
-diff_parser = subparsers.add_parser('diff')
-diff_parser.add_argument('param1')
-diff_parser.add_argument('param2')
-diff_parser.set_defaults(func=diff)
+        compress_parser = subparsers.add_parser('comp')
+        compress_parser.add_argument('param1')
+        compress_parser.set_defaults(func=compress)
 
-inter_parser = subparsers.add_parser('inter')
-inter_parser.add_argument('param1')
-inter_parser.add_argument('param2')
-inter_parser.set_defaults(func=inter)
+        diff_parser = subparsers.add_parser('diff')
+        diff_parser.add_argument('param1')
+        diff_parser.add_argument('param2')
+        diff_parser.set_defaults(func=diff)
 
-union_parser = subparsers.add_parser('union')
-union_parser.add_argument('param1')
-union_parser.add_argument('param2')
-union_parser.set_defaults(func=union)
+        inter_parser = subparsers.add_parser('inter')
+        inter_parser.add_argument('param1')
+        inter_parser.add_argument('param2')
+        inter_parser.set_defaults(func=inter)
 
-nth_parser = subparsers.add_parser('nth')
-nth_parser.add_argument('param1')
-nth_parser.add_argument('param2')
-nth_parser.set_defaults(func=nth)
+        union_parser = subparsers.add_parser('union')
+        union_parser.add_argument('param1')
+        union_parser.add_argument('param2')
+        union_parser.set_defaults(func=union)
+
+        nth_parser = subparsers.add_parser('nth')
+        nth_parser.add_argument('param1')
+        nth_parser.add_argument('param2')
+        nth_parser.set_defaults(func=nth)
 
 
-args = parser.parse_args()
-args.func(args)
+        args = parser.parse_args()
+        args.func(args)
